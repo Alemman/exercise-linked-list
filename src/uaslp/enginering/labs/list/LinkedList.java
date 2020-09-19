aa74eb0da5687f3c08d64d7a25feba294ffc4f45
Alemman <gstavo.aleman@gmail.com>
2020-09-19 03:05:42 -0500

    Estructura linkedlist creada

---
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,37 @@
+package uaslp.enginering.labs.list;
+import uaslp.enginering.labs.model.*;
+
+    public class LinkedList {
+        private Node front;
+        private Node tail;
+
+
+        public void add(Student student){
+
+        }
+
+        public void delete(Student student){
+
+        }
+
+        public void delete(int index){
+
+        }
+
+        public IteratorLinkedList getIterator(){
+            return null;
+        }
+
+        public int size(){
+            return 0;
+        }
+
+        public Student getAt(int index){
+            return null;
+        }
+
+        public void insert(Student reference,Student newStudent){
+
+        }
+    }
+
